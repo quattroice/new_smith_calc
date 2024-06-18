6c37bbe7ad1164631842aef548a919cc2d854048
quattroice <zyuuminnx@gmail.com>
2024-06-18 11:46:18 +0900

    append delta_damage

--- src/Material_state.py
+++ src/Material_state.py
@@ -12,32 +12,36 @@ class Material_state(tk.Frame):
         self.damage = tk.StringVar(value=0)
         self.min_damage = tk.IntVar(value=0)
         self.max_damage = tk.IntVar(value=0)
+        self.delta_damage = tk.IntVar(value=0)
         # 入力バーの作成
         self.entry = tk.Entry(self,width=8,state="disable",textvariable=self.damage)
         self.entry.grid(row=0,column=0,sticky="nsew",columnspan=2)
         self.entry.bind("<KeyRelease>",self.entry_bg)
         self.entry.insert(0,0)
+        # 最大値との差分ラベルの作成
+        self.delta_label = tk.Label(self,width=8,text=self.delta_damage.get(),anchor="center",relief="solid",bd=1)
+        self.delta_label.grid(row=1,column=0,sticky="nsew",columnspan=2)
         # 数値ラベルの作成
         self.label_min = tk.Label(self,width=4,text=self.min_damage.get(),anchor="center",relief="solid",bd=1)
-        self.label_min.grid(row=1,column=0,sticky="nsew")
+        self.label_min.grid(row=2,column=0,sticky="nsew")
         self.label_max = tk.Label(self,width=4,text=self.max_damage.get(),anchor="center",relief="solid",bd=1)
-        self.label_max.grid(row=1,column=1,sticky="nsew")
+        self.label_max.grid(row=2,column=1,sticky="nsew")
 
     def ms_reset(self):
         self.damage.set(0)
         self.min_damage.set(0)
         self.max_damage.set(0)
+        self.delta_damage.set(0)
         self.label_min.configure(text=self.min_damage.get())
         self.label_max.configure(text=self.max_damage.get())
+        self.delta_label.configure(text=self.delta_damage.get())
         self.entry.configure(state="disable")
         self.entry.insert(0,self.damage.get())
 
     def entry_bg(self,event):
-        dm = 0
-        try:
-            dm = int(self.damage.get())
-        except ValueError:
-            dm = 0
+        dm = self.get_damage()
+        self.delta_damage.set(self.max_damage.get() - self.get_damage())
+        self.delta_label.configure(text=self.delta_damage.get())    
         if((self.min_damage.get() <= dm)&(self.max_damage.get() >= dm)):
             self.entry.config(background="lightskyblue")
         elif(self.max_damage.get() <= dm):
@@ -58,8 +62,10 @@ class Material_state(tk.Frame):
             if res_from_table:
                 self.min_damage.set(res_from_table[0])
                 self.max_damage.set(res_from_table[1])
+                self.delta_damage.set(self.max_damage.get() - self.get_damage())
                 self.label_min.configure(text=self.min_damage.get())
                 self.label_max.configure(text=self.max_damage.get())
+                self.delta_label.configure(text=self.delta_damage.get())
                 
                 if self.max_damage.get() != 0:
                     self.entry.configure(state="normal")
@@ -68,3 +74,10 @@ class Material_state(tk.Frame):
         finally:
             cur.close()
             conn.close()
+
+    def get_damage(self):
+        try:
+            dm = int(self.damage.get())
+        except ValueError:
+            dm = 0
+        return dm
--- src/Tempature_table.py
+++ src/Tempature_table.py
@@ -11,6 +11,15 @@ class Tempature_table(tk.Frame):
 		# self.tempature = tk.IntVar(value=1000)
 		title_rows = (u"温度",u"数値１",u"数値２",u"数値３",u"数値４",u"数値５",u"数値６",u"数値７")
 		clm = ["title","value1","value2","value3","value4","value5","value6","value7"]
+		self.table_name_list = {
+			0.5:"TEMPATURE05",
+			0.8:"TEMPATURE08",
+			1.0:"TEMPATURE",
+			1.2:"TEMPATURE12",
+			2.0:"TEMPATURE20",
+			2.5:"TEMPATURE25",
+			3.0:"TEMPATURE30"
+		}
 		self.current_tempature_table = ttk.Treeview(
 			self,
 			columns=clm,
@@ -28,7 +37,8 @@ class Tempature_table(tk.Frame):
 		self.iid1 = self.current_tempature_table.insert(parent="",index="end",values=self.get_value_fromdb())
 		self.iid2 = self.current_tempature_table.insert(parent="",index="end",values=self.get_value_fromdb(tmp=0))
 
-	def get_value_fromdb(self,tp="",tmp=1000,focus=1.0):
+	def get_value_fromdb(self,tp="",tmp=1000,focus=1.0,m_focus=1.0):
+		table_name = self.table_name_list[focus]
 		# データベースへ接続
 		dbname = "../db/tempature.db"
 		db_path = os.path.join(os.path.dirname(__file__), '..', 'db', dbname)
@@ -37,9 +47,9 @@ class Tempature_table(tk.Frame):
 		if tp == "":
 			tp = str(tmp)
 		top = (tp,)
-		cur.execute("select V1,V2,V3,V4,V5,V6,V7 from TEMPATURE where TMP = {}".format(tmp))
+		cur.execute("select V1,V2,V3,V4,V5,V6,V7 from {} where TMP = {}".format(table_name,tmp))
 		res_from_table = cur.fetchone()
-		smith_damage = tuple(math.floor(focus * num)for num in res_from_table)
+		smith_damage = tuple(math.floor(m_focus * num)for num in res_from_table)
 		result = top + smith_damage
 		cur.close()
 		conn.close()
@@ -52,16 +62,16 @@ class Tempature_table(tk.Frame):
 		# 倍半（material_type = 1)なら倍半処理、そうでないなら通常倍率を１列目に
 		if((mtype == 1)&(tm % 200 == 0)):
 			if(tm % 400 == 0):
-				self.current_tempature_table.item(self.iid1,values=self.get_value_fromdb(tmp=tm,focus=focus*2.0))
+				self.current_tempature_table.item(self.iid1,values=self.get_value_fromdb(tmp=tm,focus=focus,m_focus=2.0))
 			elif(tm % 400 == 200):
-				self.current_tempature_table.item(self.iid1,values=self.get_value_fromdb(tmp=tm,focus=focus*0.5))
+				self.current_tempature_table.item(self.iid1,values=self.get_value_fromdb(tmp=tm,focus=focus,m_focus=0.5))
 		else:	
 			self.current_tempature_table.item(self.iid1,values=self.get_value_fromdb(tmp=tm,focus=focus))
 
 		# 光地金で200の倍数のみ二列目に値を挿入
 		if((mtype == 2)&(tm % 200 == 0)):
-			self.current_tempature_table.item(self.iid2,values=self.get_value_fromdb(tp="光部分",tmp=tm,focus=focus*2.0))
+			self.current_tempature_table.item(self.iid2,values=self.get_value_fromdb(tp="光部分",tmp=tm,focus=focus,m_focus=2.0))
 		elif((mtype == 1)&(tm == 1000)):
 			self.current_tempature_table.item(self.iid2,values=self.get_value_fromdb(tp="1T目",tmp=tm,focus=1.0))
 		else:
-			self.current_tempature_table.item(self.iid2,values=self.get_value_fromdb(tmp=0,focus=0))
+			self.current_tempature_table.item(self.iid2,values=self.get_value_fromdb(tmp=0))
